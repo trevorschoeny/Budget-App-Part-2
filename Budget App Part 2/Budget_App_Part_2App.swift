ae3c4b6179304ccb871c04ef8d95a9b17ec0ed9a
Trevor Schoeny <trevorschoeny1@gmail.com>
2021-05-28 11:57:01 -0400

    Initial Commit

---
+++ Budget App Part 2/Budget_App_Part_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_App_Part_2App.swift
+//  Budget App Part 2
+//
+//  Created by Trevor Schoeny on 5/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Budget_App_Part_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
